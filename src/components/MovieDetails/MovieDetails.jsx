b12dd3ff15a00a8090635ea2d2ac6366af486c63
daniarabykeev <daniarabykeev@gmail.com>
2023-04-26 22:58:05 +0600

    fix: bug fixed

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -22,49 +22,56 @@ const MovieDetails = () => {
 
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <div className="movie-title">{data.Title}</div>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <i className="fa fa-star"> : {data.imdbRating}</i>
-          </span>
-          <span>
-            IMDB Votes <i className="fa fa-thumbs-up"> : {data.imdbVotes}</i>
-          </span>
-          <span>
-            Runtime <i className="fa fa-film"> : {data.Runtime}</i>
-          </span>
-          <span>
-            Year <i className="fa fa-calendar"> : {data.Year}</i>
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director:</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).length === 0 ? (
+        <div>...loading</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i className="fa fa-star"> : {data.imdbRating}</i>
+              </span>
+              <span>
+                IMDB Votes{" "}
+                <i className="fa fa-thumbs-up"> : {data.imdbVotes}</i>
+              </span>
+              <span>
+                Runtime <i className="fa fa-film"> : {data.Runtime}</i>
+              </span>
+              <span>
+                Year <i className="fa fa-calendar"> : {data.Year}</i>
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director:</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars:</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Generes:</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Languages:</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards:</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars:</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Generes:</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages:</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards:</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
